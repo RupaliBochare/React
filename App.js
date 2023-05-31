56e7e548b3f970e44d68c0050dd464ea0de3464b
Rupali Bochare <rupali_bochare_tp@bmc.com>
2023-05-31 17:49:37 +0530

    episode number 2 code

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-//creating nesting div structure
-//like below
-/*
-<div id ="parent">
-  <div id="child">
-  <h1>I in H1 tag<h1>
-  </div>
+// import React from "react"
+// import { ReactDOM } from "react-dom/client";
 
-</div>
-*/ 
-const heading = React.createElement(
-  "div",
-  { id: "parent", className: "customStyle" },
-  React.createElement("div", {id: "child"},
-  React.createElement("h1", {id: "child"}, "I am in nesting h1")
-));
-const rootEle = ReactDOM.createRoot(document.getElementById("test"));
-rootEle.render(heading);
+// /*
+// <div id ="parent">
+//   <div id="child">
+//   <h1>I in H1 tag<h1>
+//   </div>
+
+// </div>
+// */ 
+// // const heading1 = React.createElement(
+// //   "div",
+// //   { id: "parent", className: "customStyle" },
+// //   React.createElement("div", {id: "child"},
+// //   React.createElement("h1", {id: "child"}, "I am in nesting h1")
+// // ));
+// const heading = React.createElement("h1", {}, "I am in nesting h1");
+// const rootEle = rootReactDOM.createRoot(document.getElementById('root'))
+
+// rootEle.render(heading);
+
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {}, "I'm in h1 tag "),
+    React.createElement("h1", {}, "I'm in h1 tag "),
+    ]),
+    React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "I'm in h1 of child 2tag "),
+    React.createElement("h1", {}, "I'm in h1  of child 2 sibling tag "),
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
